c69bd5d819332f4e41a87b66e01af3bb35491085
João Vitor <joaocontatoprofissional@hotmail.com>
2025-06-09 00:01:41 -0300

    Criada pasta resouces para ficar o arquivo de configuração do bd alterada classe ConnectionBanco

--- my-easy-real/src/main/java/connection/ConnectionBanco.java
+++ my-easy-real/src/main/java/connection/ConnectionBanco.java
@@ -1,19 +1,39 @@
 package connection;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 /* Essa classe criar uma nova conexão com o banco a cada chamada do getConnection()
 para utilizar o try-with-resources  com segurança*/
 
 public class ConnectionBanco {
-	private static String urlBanco = "jdbc:postgresql://localhost:5432/my_easy_real?autoReconnect=true";;
-	private static String user = "postgres";
-	private static String password ="admin";
+	
+	private static String urlBanco ;
+	private static String user ;
+	private static String password ;
+	
+	
 	
 	
 	static {
 		try {
 			Class.forName("org.postgresql.Driver");
+			
+			Properties props = new Properties();
+			InputStream input = ConnectionBanco.class.getClassLoader().getResourceAsStream("config.properties");
+			
+			if(input==null) {
+				throw new RuntimeException("Arquivo config.properties não encontrado no classpath.");
+			}
+			
+			props.load(input);
+			
+			urlBanco = props.getProperty("db.url");
+			user = props.getProperty("db.user");
+			password = props.getProperty("db.password");
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("Erro ao carregar o driver do banco de dados"+
